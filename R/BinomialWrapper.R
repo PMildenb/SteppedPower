e46abec9e7de3cc5e3d7d57b5338aa702e557599
Philipp Mildenberger <pmildenb@uni-mainz.de>
2018-11-15 16:13:31 +0100

    wlsGlmmPower

--- R/BinomialWrapper.R
+++ R/BinomialWrapper.R
@@ -1,7 +1,12 @@
 #'  'wlsGlmmPower'
 #'
 #'
-#'
+#' @param I integer (vector), number of clusters per wave (in SWD)
+#' @param
+#' @param tau numeric, standard deviation of random intercepts
+#' @param sig.level numeric, significance level, defaults to 0.05
+#' @param delay numeric (possibly vector), value between 0 and 1 specifing the
+#' intervention effect in the first (second ... ) intervention phase
 #'
 #'
 #'
@@ -13,14 +18,14 @@
 ## how to handle tau,eta,rho (delta-method?) ?
 
 wlsGlmmPower <- function(I,mu0,mu1,tau,eta=NULL,rho=NULL,
-                                family="binomial",N=NULL,sig.level=0.05){
+                                family="binomial",N=NULL,sig.level=0.05,delay=NULL){
   if(family =="binomial"){
     Sigmas  <- c(sigma0=sqrt(mu0*(1-mu0)),sigma1=sqrt(mu1*(1-mu1)) )
     EffSize <- mu1-mu0  ; OR <- (mu1*(1-mu0))/(mu0*(1-mu1))
     print(paste("The assumed odds ratio is",round(OR,4)))
 
     wlsMixedPower(EffSize=EffSize,I=I,sigma=Sigmas,tau=tau,
-                family=family,N=N,sig.level=sig.level)
+                family=family,N=N,sig.level=sig.level,delay=delay)
   }
 }
 
