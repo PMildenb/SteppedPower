d7450631519a2cd2f217ec9ecc928141b2f9215b
Philipp Mildenberger <pmildenb@uni-mainz.de>
2018-11-20 19:59:19 +0100

    test files

--- tests/testthat/test_CovBlk.R
+++ tests/testthat/test_CovBlk.R
@@ -6,4 +6,6 @@ context("Check CovBlk()")
 expect_equal(construct_CovBlk(3,2,3),
              matrix(c(13,9,9,9,13,9,9,9,13),nrow=3))
 
-expect_equal()
+expect_equal(
+  construct_CovBlk(3,2,c(1,2,3))
+  )
