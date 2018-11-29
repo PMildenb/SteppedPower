fff3a1d26012c45acb9bfcbbe9cd77e1af653801
Philipp Mildenberger <pmildenb@uni-mainz.de>
2018-11-29 13:01:49 +0100

    full function names now in plotwls  , too ...

--- R/plot_wlsPower.R
+++ R/plot_wlsPower.R
@@ -12,18 +12,20 @@ plot_wlsPower <- function(wlsPower){
   HatMat <- wlsPower$HatMat
   HatData <- reshape2::melt(t(HatMat[c(nrow(HatMat):1),]))
   names(HatData) <- c("Time","Cluster","Weight")
-  plotraw <- ggplot2::ggplot(HatData,aes(Time,Cluster)) +
-    theme_minimal()  + scale_fill_gradient2(low="steelblue",mid="white",high="red") +
-    geom_tile(aes(fill=Weight),colour="white")
+  plotraw <- ggplot2::ggplot(HatData,ggplot2::aes(Time,Cluster)) +
+    ggplot2::theme_minimal()  + ggplot2::scale_fill_gradient2(low="steelblue",mid="white",high="red") +
+    ggplot2::geom_tile(ggplot2::aes(fill=Weight),colour="white")
 
 
   Timeweights    <- colSums(abs(HatMat))
   plotCluster <- ggplot2::ggplot(data.frame(Cluster=1:dim(HatMat)[1],
                                             Clusterweights=rowSums(abs(HatMat))),
-                                 aes(Cluster,Clusterweights)) + geom_point() + theme_minimal()
+                                 ggplot2::aes(Cluster,Clusterweights)) +
+    ggplot2::geom_point() + ggplot2::theme_minimal()
   plotPeriods <- ggplot2::ggplot(data.frame(Periods=1:dim(HatMat)[2],
                                              Weights=colSums(abs(HatMat))),
-                                 aes(Periods,Weights)) + geom_point() + theme_minimal()
+                                 ggplot2::aes(Periods,Weights)) +
+    ggplot2::geom_point() + ggplot2::theme_minimal()
 
   return(list(plotraw,plotCluster,plotPeriods))
 }
