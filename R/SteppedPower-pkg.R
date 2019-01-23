ad61e542ec9c76cf6c352128f92307499e4ef1ad
Philipp Mildenberger <pmildenb@uni-mainz.de>
2019-01-23 17:06:23 +0100

    roxygenised

--- R/SteppedPower-pkg.R
+++ R/SteppedPower-pkg.R
@@ -2,7 +2,8 @@
 #'
 #' Assesses Power of mixed models for the evaluation of a intervention.
 #'
-#' @import Matrix ggplot2 reshape2 gee swCRTdesign stats
+#' @import stats
+#' @import Matrix ggplot2 reshape2 gee swCRTdesign
 #' @author Philipp Mildenberger \email{pmildenb@@uni-mainz.de}
 #' @name SteppedPower-pkg
 #' @docType package
