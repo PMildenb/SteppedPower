8916825499d93f4c52241d83df15f9d8e30c8634
Philipp Mildenberger <pmildenb@uni-mainz.de>
2019-01-28 16:50:20 +0100

    construct DesMat tuned: trt_delay and time_adjust work

--- R/construct_DesMat.R
+++ R/construct_DesMat.R
@@ -5,8 +5,9 @@
 #' Note: Unlike the usual notiation, the treatment is in the first column (for easier access by higher level functions).
 #'
 #' @param Cl integer (vector), number of clusters per wave (in SWD)
-#' @param delay *not implemented*
+#' @param trt_delay *implemented for swd, parallel and parallel w baseline*
 #' @param timepoints numeric, scalar
+#' @param time_adjust character, specifies adjustment for time periods. Defaults to "factor".
 #'
 #' @return a matrix (for a stepped wedge design)
 #' @export
@@ -14,7 +15,7 @@
 #' @examples construct_DesMat(Cl=c(2,0,1))
 #'
 
-construct_DesMat <- function(Cl,delay=NULL,design="SWD",timepoints=NULL){
+construct_DesMat <- function(Cl,trt_delay=NULL,design="SWD",timepoints=NULL,time_adjust="factor"){
 
   SumCl         <- sum(Cl)
 
@@ -22,18 +23,28 @@ construct_DesMat <- function(Cl,delay=NULL,design="SWD",timepoints=NULL){
     sequences     <- length(Cl)
     if(is.null(timepoints)) timepoints <- sequences + 1  ## hier modifikationen moeglich
     trt    <- matrix(0,sequences,timepoints) ; trt[upper.tri(trt)] <- 1
+    if(!is.null(trt_delay)){
+      for(i in 1:length(trt_delay)){
+        diag(trt[,-(1:i)]) <- trt_delay[i]  ## doesnt work if length(delay)>=length(timepoints)-1
+      }
+    }
     trtBlk <- trt[rep(1:sequences,Cl),]
     trtvec <- as.numeric(t(trtBlk))
   }else
   if(design=="parallel"){
-    if(length(Cl)!=2) {stop("Cannot handle length of vector Cl")}
+    if(length(Cl)!=2) {stop("In construct_DesMat: Cannot handle length of vector Cl")}
     if(is.null(timepoints)){
-      timepoints <- 1 ;  warning("timepoints unspecified. Defaults to 1.")
+      if(is.null(trt_delay)){
+        timepoints <- 1 ;  warning("timepoints unspecified. Defaults to 1.")
+      }else
+        timepoints <- length(trt_delay)+1 ;  message("timepoints unspecified. Defaults to length(trt_delay)+1")
     }
-    trtvec  <- rep(c(0,1),Cl*timepoints)
+    ctl_cluster <- rep(0,timepoints)
+    trt_cluster <- c(trt_delay,rep(1,(timepoints-length(trt_delay))))
+    trtvec      <- c(rep(ctl_cluster,Cl[1]),rep(trt_cluster,Cl[2]))
   }else
   if(design=="parallel_baseline"){
-    if(length(Cl)!=2) {stop("Cannot handle length of vector Cl")}
+    if(length(Cl)!=2) {stop("In construct_DesMat: Cannot handle length of vector Cl")}
     if(length(timepoints)==1){
       timepoints01 <- c(1,timepoints-1)
       message(paste("assumes 1 baseline period and",timepoints-1,"parallel period(s)"))
@@ -45,12 +56,20 @@ construct_DesMat <- function(Cl,delay=NULL,design="SWD",timepoints=NULL){
       timepoints   <- 2
       message("timepoints unspecified. Defaults to 1 baseline, 1 parallel period.")
     }
-    trtvec  <- c(rep(0,timepoints*Cl[1]),
-                 rep(rep(c(0,1),timepoints01),Cl[2]))
+    ctl_cluster <- rep(0,sum(timepoints01))
+    trt_cluster <- c(rep(0,timepoints01[1]),
+                     trt_delay,rep(1,(timepoints01[2]-length(trt_delay))))
+    trtvec      <- c(rep(ctl_cluster,Cl[1]),rep(trt_cluster,Cl[2]))
   }
 
-  timeBlk <- suppressWarnings(cbind(1,rbind(0,diag(1,timepoints-1))))
-  timeBlk <- timeBlk[rep(1:timepoints,SumCl),]
+  if(time_adjust=="factor"){
+    timeBlk <- suppressWarnings(cbind(1,rbind(0,diag(1,timepoints-1))))
+    timeBlk <- timeBlk[rep(1:timepoints,SumCl),]
+  } else
+  if(time_adjust=="None"){
+    timeBlk <- matrix(rep(1,timepoints*SumCl))
+  }
+
   DesMat  <- list(cbind(trtvec,timeBlk),timepoints,SumCl)
   names(DesMat) <- c("matrix","timepoints","SumCl")
   return(DesMat)
