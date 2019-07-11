064d49af0461d24956c4b2dacde14a58f733fcb3
Philipp Mildenberger <pmildenb@uni-mainz.de>
2019-07-11 04:48:07 +0200

    power for iscb

--- R/icsb_poster_analytical.R
+++ R/icsb_poster_analytical.R
@@ -3,14 +3,21 @@
 
 RUNS <- seq(0,1,length.out=11)
 Power <- list()
-for(tau in RUNS){
-  Power[[paste0("_",tau)]] <- wlsGlmmPower(Cl=c(2,2,2,2,2,2),mu0=0.03, mu1=0.02, time_adjust="factor",
-                                           tau_lin = tau,trt_delay=.5, N=250,verbose=T)$Power
+for(run in RUNS){
+  Power[[paste0("_",run)]] <- wlsGlmmPower(Cl=c(2,2,2,2,2,2),mu0=0.03, mu1=.02, time_adjust="factor",
+                                           tau_lin = run,trt_delay=.5, N=250,verbose=T)$Power
 }
-
 Power
 
 
-wlsGlmmPower(Cl=c(2,2,2,2,2,2),mu0=0.03, mu1=0.02, time_adjust="factor",
-             tau_lin = 0.104,trt_delay=.5, N=250,verbose=T)$Power
+
+RUNS <- seq(0.03,0.02,length.out=11)
+Power <- list()
+for(run in RUNS){
+  Power[[paste0("_",run)]] <- wlsGlmmPower(Cl=c(2,2,2,2,2,2),mu0=0.03, mu1=run, time_adjust="factor",
+                                           tau_lin = .1,trt_delay=.5, N=250,verbose=T)$Power
+}
+Power
+
+
 
