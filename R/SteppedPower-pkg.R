267c6fbd464ed6b071f625c74b4af66b3766206f
Ph Mildenb <philipp.mildenberger@mail.de>
2020-08-06 16:32:50 +0200

    updated dependencies

--- R/SteppedPower-pkg.R
+++ R/SteppedPower-pkg.R
@@ -4,13 +4,9 @@
 #'
 #' @importFrom stats coef family gaussian optim pnorm qnorm rbinom rnorm uniroot pt qt
 #' @import Matrix
-#' @importFrom ggplot2 aes_string geom_point geom_tile ggplot scale_fill_gradient2
-#' theme_minimal
-#' @import reshape2
-#' @importFrom swCRTdesign swPwr
 #' @importFrom car logit
-#' @importFrom graphics plot
-#' @importFrom stats dnorm integrate
+#' @import plotly
+#'
 #'
 #' @author Philipp Mildenberger \email{pmildenb@@uni-mainz.de}
 #' @name SteppedPower-pkg
